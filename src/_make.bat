52af238c48488271aab940a819ecd26f07a53c69
tappy <hopping.tappy@gmail.com>
2020-02-20 22:51:15 +0900

    nsc をコマンドラインでビルド

--- src/_make.bat
+++ src/_make.bat
@@ -1,9 +1,26 @@
+
+mkdir ..\bin
+pushd	..\bin
+	del /q *.*
+popd
+
 py freqTableGen.py -f 442 -o nsd\
 rem --- nsd.lib ---
 pushd nsd
 	call _make
 	copy *.lib ..\..\lib\
 popd
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+
+cd %~dp0
+MSBuild "nsc\nsc.sln" /t:clean;rebuild /p:Configuration=Release;Platform="x86"
+if %ERRORLEVEL% neq 0 (
+	echo ErrorLevel:%ERRORLEVEL%
+	echo ƒrƒ‹ƒhŽ¸”s
+)
+
+
 
 rem --- nsc.exe ---
 pushd nsc\release
